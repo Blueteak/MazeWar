4ccb64f2e757712af3b2a04b26f85336661fc649
Jonathan Schenker <jonathan.f.schenker@vanderbilt.edu>
2016-01-23 13:59:24 -0600

    Completed UI Layout
    • Pixel perfect UI layout based on GameUI.md
    • Changed Arrow icon on minimap for better size
    • Set Camera to fit into window box regardless of screen resolution
    • Black bars obscure outer area on Aspect Ratios different than 3:4

---
+++ MazeWar/Assets/GameScripts/PlayerCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour {
+
+	RectTransform c;
+	RectTransform r;
+	public Vector2 dim;
+	public Vector2 offset;
+
+	// Use this for initialization
+	void Start () 
+	{
+		c = FindObjectOfType<Canvas>().GetComponent<RectTransform>();
+		r = GameObject.FindGameObjectWithTag("PlayerWindow").GetComponent<RectTransform>();
+		dim = new Vector2(r.sizeDelta.x/c.rect.width, r.sizeDelta.y/c.rect.height);
+		offset = new Vector2(((c.rect.width-r.sizeDelta.x)/2)/c.rect.width,(c.rect.height-(c.rect.height*0.0247f)-r.sizeDelta.y)/c.rect.height);
+		GetComponent<Camera>().rect = new Rect(offset.x, offset.y, dim.x, dim.y);
+		dim = new Vector2(dim.x*c.rect.width, dim.y*c.rect.height);
+		offset = new Vector2(offset.x*c.rect.width, offset.y*c.rect.height);
+	}
+
+
+}
